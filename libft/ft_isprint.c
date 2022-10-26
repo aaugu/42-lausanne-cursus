8247b7df6fdd472704948d8f7dc21301b94643fa
Anais Augu <augu.anais@gmail.com>
2022-10-26 14:55:15 +0200

    ft_isprint ok

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -6,14 +6,13 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 12:02:15 by aaugu             #+#    #+#             */
-/*   Updated: 2022/10/26 14:24:29 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/10/26 14:55:06 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
 #include <ctype.h>
 #include <stdio.h>
-*/
+
 int	ft_isprint(int c)
 {
 	if (c >= 32 || c <= 126)
