48635959618aa08e8486912236effb60a78392aa
Anais Augu <augu.anais@gmail.com>
2022-10-28 15:53:44 +0200

    ft_memchr in progress

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,10 +6,24 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 12:02:18 by aaugu             #+#    #+#             */
-/*   Updated: 2022/10/27 14:26:01 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/10/28 15:52:03 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+	unsigned char	*str
+
+	i = 0;
+	str = (unsigned char *)s
+	if (!s)
+		return (0);
+	while (i++ < n)
+	{
+		if (str[i] == c)
+			return (s[i]);
+	}
+}
--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,14 +6,31 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 12:02:24 by aaugu             #+#    #+#             */
-/*   Updated: 2022/10/28 14:06:35 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/10/28 15:28:46 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
-void	*ft_memmove(void *dst, const void *src, size_t len);
-{
 
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char	*s;
+	char	*d;
+	size_t	i;
+
+	s = (char *)src;
+	d = (char *)dst;
+	i = 0;
+	if (!dst && !src)
+	return (0);
+	if (d > s)
+		while (len-- > 0)
+			d[len] = s[len];
+	else
+		while (i < len)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	return (dst);
 }
-*/
