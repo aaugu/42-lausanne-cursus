8736254a13b689d1a591f035cf4f8abe6820170f
Anais Augu <augu.anais@gmail.com>
2022-11-03 09:36:18 +0100

    strtrim in progress

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,10 +6,41 @@
 /*   By: aaugu <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 13:42:35 by aaugu             #+#    #+#             */
-/*   Updated: 2022/10/27 11:57:59 by aaugu            ###   ########.fr       */
+/*   Updated: 2022/11/02 14:03:42 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set);
+int	count_set_start(char const *s1, char const *set);
+int	count_set_end(char const *s1, char const *set);
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		start;
+	int		end;
+	int		i;
+	char	*str;
+
+	start = count_set_start(s1, set);
+	end = count_set_end(s1, set);
+	if (ft_strlen(s1) - end - start > 0)
+		str = (char *)malloc(sizeof(char) * (ft_strlen(s1) - end - start + 1));
+	else
+		return (ft_strdup(""));
+	if (!str)
+		return (0);
+	i = 0;
+	while (start - 1 < end - 1)
+	{
+		str[i] = s1[start - 1];
+		i++;
+		start++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+int	count_set_start(char const *s1, char const *set);
+
+int	count_set_end(char const *s1, char const *set);
